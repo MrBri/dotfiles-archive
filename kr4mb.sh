35d621f718436c74ae5863dcd10509a97e879aaa
Tyler Briles <tyler@mrbri.com>
2014-04-04 20:01:53 -0700

    Global .gitconfig, Using Alfred, kr4mb and pckh settings

---
+++ kr4mb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+
+$cli set repeat.wait 33
+/bin/echo -n .
+$cli set repeat.initial_wait 300
+/bin/echo -n .
+$cli set bilalh.remap.f19_escape_control 1
+/bin/echo -n .
+/bin/echo
